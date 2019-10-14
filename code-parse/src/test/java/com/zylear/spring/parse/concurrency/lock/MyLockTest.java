c610502e998ec27fedde43314d9c0dd2c79ba51b
zylear <284449710@qq.com>
2019-10-14 11:09:48 +0800

    countDownLatch cyclicBarrier

--- code-parse/src/test/java/com/zylear/spring/parse/concurrency/lock/MyLockTest.java
+++ code-parse/src/test/java/com/zylear/spring/parse/concurrency/lock/MyLockTest.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -11,10 +14,15 @@ import static org.junit.Assert.*;
 
 public class MyLockTest {
 
-	static int index = 0;
+
+
+	static volatile int index = 0;
 
 	@Test
-	public void withoutLockTest() {
+	public void withoutLockTest() throws InterruptedException {
+		//等到所有线程完成再退出
+		CountDownLatch countDownLatch = new CountDownLatch(1000);
+
 		List<Thread> list = new LinkedList<>();
 		long l = System.currentTimeMillis();
 		for (int i = 0; i < 1000; i++) {
@@ -28,18 +36,13 @@ public class MyLockTest {
 						e.printStackTrace();
 					}
 					index = i + 1;
+					countDownLatch.countDown();
 				}
 			});
 			thread.start();
 			list.add(thread);
 		}
-		for (Thread thread : list) {
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+		countDownLatch.await();
 
 		System.out.println(index);
 		System.out.println(System.currentTimeMillis() - l);
@@ -47,7 +50,8 @@ public class MyLockTest {
 	}
 
 	@Test
-	public void lockTest() {
+	public void lockTest() throws InterruptedException {
+		CountDownLatch countDownLatch = new CountDownLatch(1000);
 		Lock myLock = new MyLock();
 		List<Thread> list = new LinkedList<>();
 		long l = System.currentTimeMillis();
@@ -64,28 +68,39 @@ public class MyLockTest {
 							e.printStackTrace();
 						}
 						index = i + 1;
+					} catch (Exception e) {
+						e.printStackTrace();
 					} finally {
 						myLock.unlock();
 					}
+					countDownLatch.countDown();
 
 				}
 			});
 			thread.start();
 			list.add(thread);
 		}
-		for (Thread thread : list) {
-			try {
-				thread.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+
+
+		countDownLatch.await();
+//		for (Thread thread : list) {
+//			try {
+//				thread.join();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		cyclicBarrier.
+
+
 		System.out.println(index);
 		System.out.println(System.currentTimeMillis() - l);
 	}
 
 	@Test
-	public void rLockTest() {
+	public void rLockTest() throws InterruptedException {
+		CountDownLatch countDownLatch = new CountDownLatch(1000);
+
 		Lock myLock = new ReentrantLock();
 		List<Thread> list = new LinkedList<>();
 		long l = System.currentTimeMillis();
@@ -104,6 +119,44 @@ public class MyLockTest {
 						index = i + 1;
 					} finally {
 						myLock.unlock();
+					}
+					countDownLatch.countDown();
+
+				}
+			});
+			thread.start();
+			list.add(thread);
+		}
+		countDownLatch.await();
+//		for (Thread thread : list) {
+//			try {
+//				thread.join();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		System.out.println(index);
+		System.out.println(System.currentTimeMillis() - l);
+	}
+
+	@Test
+	public void cyclicBarrierTest() {
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
+		List<Thread> list = new LinkedList<>();
+		long l = System.currentTimeMillis();
+		for (int i = 0; i < 10; i++) {
+			int finalI = i;
+			Thread thread = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						System.out.println(finalI +" ready");
+						cyclicBarrier.await();
+						System.out.println(finalI +" go");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (BrokenBarrierException e) {
+						e.printStackTrace();
 					}
 
 				}
